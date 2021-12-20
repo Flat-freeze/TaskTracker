0bf9f11d1d91f8c2c8f011da7b4ec658f243fcce
NerveFalcon <suraevoleg@gmail.com>
2021-12-20 19:51:19 +0300

    Add some Models

---
+++ src/TaskTracker.Models/Filtrers/TitleFilter.cs
@@ -0,0 +1,23 @@
+﻿using AutoFilter;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Models.Filtrers
+{
+    public class TitleFilter
+    {
+        [FilterProperty(IgnoreCase = true,
+                          TargetPropertyName = "Title",
+                          StringFilter = StringFilterCondition.Contains
+                        )]
+        public string? Title { get; set; }
+        [NotAutoFiltered]
+        public string? OrderBy { get; set; }
+        [NotAutoFiltered]
+        public bool Desk { get; set; }
+    }
+}
